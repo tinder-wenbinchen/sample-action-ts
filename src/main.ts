5a5de18a326a65805d606f8af8456dae5b04aea4
wenbinchen <wenbin.chen@gotinder.com>
2024-03-21 17:46:49 -0400

    adding github api in action

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github';
+import { context, getOctokit } from '@actions/github'
 import { wait } from './wait'
 
 /**
@@ -9,6 +9,26 @@ import { wait } from './wait'
 export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    const { owner, repo } = context.repo
+    const issue =
+      context.payload.issue?.number ??
+      context.payload.pull_request?.number ??
+      -1
+    if (issue === -1) {
+      console.warn(
+        'Was not able to determine the related PR/Issue will perform NoOp'
+      )
+      return
+    }
+    const client = getOctokit(
+      `Bearer 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`
+    )
+    const { data } = await client.rest.issues.createComment({
+      issue_number: issue,
+      owner,
+      repo,
+      body: `An friendly hello from ${context.action} and thanks for raising a PR.`
+    })
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
@@ -18,6 +38,7 @@ export async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+    core.setOutput('comment-id', data.id)
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
