3df14d4701edc0caa7f8e894d2a354c0dcebb540
wenbinchen <wenbin.chen@gotinder.com>
2024-04-01 17:00:11 -0400

    change for v0.3.12

--- dist/index.js
+++ dist/index.js
@@ -29008,7 +29008,6 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
-const wait_1 = __nccwpck_require__(5259);
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -29016,7 +29015,8 @@ const wait_1 = __nccwpck_require__(5259);
 async function run() {
     try {
         const token = core.getInput('repo-token', { required: true });
-        const ms = core.getInput('milliseconds');
+        const updateStrings = core.getInput('updatedStrings');
+        console.log(`updated strings from shell ${updateStrings}.`);
         const client = github.getOctokit(token);
         const pullRequest = github.context.payload.pull_request;
         //     const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -29029,12 +29029,26 @@ async function run() {
         //       git fetch origin `${ pullRequest.base?.ref }`
         //     }
         const issue = pullRequest.number;
-        // const { data } = await client.rest.issues.createComment({
+        // const [testData, data] = await Promise.all([
+        //   client.rest.issues.createComment({
+        //     issue_number: issue,
+        //     owner: github.context.repo.owner,
+        //     repo: github.context.repo.repo,
+        //     body: `An friendly hello from ${github.context.action} and thanks for raising a PR.`
+        //   }),
+        //   client.rest.pulls.get({
+        //     pull_number: issue,
+        //     owner: github.context.repo.owner,
+        //     repo: github.context.repo.repo
+        //   })
+        // ])
+        // const { testData } = await client.rest.issues.createComment({
         //   issue_number: issue,
         //   owner: github.context.repo.owner,
         //   repo: github.context.repo.repo,
         //   body: `An friendly hello from ${github.context.action} and thanks for raising a PR.`
-        // });
+        // })
+        // console.log(`Adding comment ${testData}.`)
         const { data } = await client.rest.pulls.get({
             pull_number: issue,
             owner: github.context.repo.owner,
@@ -29043,14 +29057,10 @@ async function run() {
         const prData = JSON.stringify(data, undefined, 2);
         console.log(`Checking pr data ${prData}.`);
         // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
         console.log(`Starting this action which is written in TS`);
         // Log the current timestamp, wait, then log the new timestamp
         core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
         core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('comment-id', data.id);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
@@ -29059,31 +29069,6 @@ async function run() {
     }
 }
 exports.run = run;
-
-
-/***/ }),
-
-/***/ 5259:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -9,7 +8,8 @@ import { wait } from './wait'
 export async function run(): Promise<void> {
   try {
     const token = core.getInput('repo-token', { required: true })
-    const ms: string = core.getInput('milliseconds')
+    const updateStrings: string = core.getInput('updatedStrings')
+    console.log(`updated strings from shell ${updateStrings}.`)
     const client = github.getOctokit(token)
     const pullRequest = github.context.payload.pull_request
     //     const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -24,12 +24,27 @@ export async function run(): Promise<void> {
     //       git fetch origin `${ pullRequest.base?.ref }`
     //     }
     const issue = pullRequest.number
-    // const { data } = await client.rest.issues.createComment({
+    // const [testData, data] = await Promise.all([
+    //   client.rest.issues.createComment({
+    //     issue_number: issue,
+    //     owner: github.context.repo.owner,
+    //     repo: github.context.repo.repo,
+    //     body: `An friendly hello from ${github.context.action} and thanks for raising a PR.`
+    //   }),
+    //   client.rest.pulls.get({
+    //     pull_number: issue,
+    //     owner: github.context.repo.owner,
+    //     repo: github.context.repo.repo
+    //   })
+    // ])
+    // const { testData } = await client.rest.issues.createComment({
     //   issue_number: issue,
     //   owner: github.context.repo.owner,
     //   repo: github.context.repo.repo,
     //   body: `An friendly hello from ${github.context.action} and thanks for raising a PR.`
-    // });
+    // })
+
+    // console.log(`Adding comment ${testData}.`)
 
     const { data } = await client.rest.pulls.get({
       pull_number: issue,
@@ -42,16 +57,11 @@ export async function run(): Promise<void> {
     console.log(`Checking pr data ${prData}.`)
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
     console.log(`Starting this action which is written in TS`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('comment-id', data.id)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
